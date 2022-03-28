e65c971c0ae58f5425ff62e344912a86d6971919
Sophie Archer <hello@sophiearcher.co.uk>
2022-03-28 22:09:06 +0100

    js text added around homepage img

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,17 @@
-console.log("hello world");
+// circular text homepage
+
+function circularText(txt, radius, classIndex) {
+  txt = txt.split(""),
+    classIndex = document.getElementsByClassName("circTxt")[classIndex];
+
+  var deg = 360 / txt.length,
+    origin = 0;
+
+  txt.forEach((ea) => {
+    ea = `<p style='height:${radius}px;position:absolute;transform:rotate(${origin}deg);transform-origin:0 100%'>${ea}</p>`;
+    classIndex.innerHTML += ea;
+    origin += deg;
+  });
+}
+
+circularText("hello@sophiearcher.co.uk ", 360, 0);
